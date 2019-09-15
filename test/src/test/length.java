448f7fdae2c8ac7d3664f4639294deb488113fc4
Moses Lara <mlara417@gmail.com>
2019-09-15 14:33:15 -0500

    added java sandbox file

---
+++ test/src/test/length.java
@@ -0,0 +1,28 @@
+package test;
+
+public class length {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x;
+		
+		x=3;
+		
+		switch (x)
+		 {
+		 case 3 : x += 1;
+		 case 4 : x += 2;
+		 case 5 : x += 3;
+		 case 6 : x++;
+		 case 7 : x += 2;
+		 case 8 : x--;
+		 case 9 : x++;
+		 }
+		
+		System.out.println (x);
+
+		
+	}
+
+}
