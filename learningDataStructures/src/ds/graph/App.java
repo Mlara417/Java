2d4d7b1809206334587b1585cb8c5a73372a7e6e
Moses Lara <mlara417@gmail.com>
2020-01-25 17:00:03 -0600

    practiced graphs ds

---
+++ learningDataStructures/src/ds/graph/App.java
@@ -0,0 +1,14 @@
+package ds.graph;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Graph myGraph = new Graph(5);
+		
+		myGraph.addEdge(1,3);
+		
+
+	}
+
+}
